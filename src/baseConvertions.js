79a3230c01a369bf8b50ef99b040effd5175115b
Will Olson <will.olson11@gmail.com>
2017-09-05 16:23:34 -0700

    refactor: Extract out base convertions

---
+++ src/baseConvertions.js
@@ -0,0 +1,34 @@
+/******* TO SNAKE *******/
+
+export const fromCamel = (string) => (
+  string.replace(/([A-Z])/g, letter => `_${letter.toLowerCase()}`)
+);
+
+export const fromConstant = (string) => (
+  string.toLowerCase()
+);
+
+export const fromPascal = (string) => (
+  string.replace(/([A-Z])/g, letter => `_${letter.toLowerCase()}`).substr(1)
+);
+
+
+/******* FROM SNAKE *******/
+
+export const toCamel = (string) => (
+  string.replace(/(_[a-z])/g, letter => letter[1].toUpperCase())
+);
+
+export const toConstant = (string) => (
+  string.toUpperCase()
+);
+
+export const toPascal = (string) => (
+  string
+    .replace(/(_[a-z])/g, letter => letter[1].toUpperCase())
+    .replace(/(^[a-z])/g, letter => letter.toUpperCase())
+);
+
+export const toTrim = (string) => (
+  string.replace(/(_)/g, '')
+);
--- src/index.js
+++ src/index.js
@@ -1,44 +1,9 @@
-/******* TO SNAKE *******/
-
-export const fromCamel = (string) => (
-  string.replace(/([A-Z])/g, letter => `_${letter.toLowerCase()}`)
-);
-
-export const fromConstant = (string) => (
-  string.toLowerCase()
-);
-
-export const fromPascal = (string) => (
-  string.replace(/([A-Z])/g, letter => `_${letter.toLowerCase()}`).substr(1)
-);
-
-
-/******* FROM SNAKE *******/
-
-export const toCamel = (string) => (
-  string.replace(/(_[a-z])/g, letter => letter[1].toUpperCase())
-);
-
-export const toConstant = (string) => (
-  string.toUpperCase()
-);
-
-export const toPascal = (string) => (
-  string
-    .replace(/(_[a-z])/g, letter => letter[1].toUpperCase())
-    .replace(/(^[a-z])/g, letter => letter.toUpperCase())
-);
-
-export const toTrim = (string) => (
-  string.replace(/(_)/g, '')
-);
-
-
-/******* FINAL CONVERSION *******/
+// Project Assets
+import * as base from './baseConvertions';
 
 export const convertParams = (input) => (
   Object.keys(input).reduce((obj, key) => {
-    obj[fromCamel(key)] = input[key]
+    obj[base.fromCamel(key)] = input[key]
     return obj
   }, {})
 );
