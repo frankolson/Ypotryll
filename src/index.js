2ee594f830afe3b3c16752adb04c60174f07377b
Will Olson <will.olson11@gmail.com>
2017-09-05 13:07:24 -0700

    refactor: Begin new function naming convention

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-export const camelToSnake = (string) => (
+/******* TO SNAKE *******/
+
+export fromCamel = (string) => (
   string.replace(/([A-Z])/g, letter => `_${letter.toLowerCase()}`)
 );
 
+
+/******* FROM SNAKE *******/
+
+
+/******* FINAL CONVERSION *******/
+
 export const convertParams = (input) => (
   Object.keys(input).reduce((obj, key) => {
-    obj[camelToSnake(key)] = input[key]
+    obj[fromCamel(key)] = input[key]
     return obj
   }, {})
 );
