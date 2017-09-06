a67472631bb4769bc41a086e927bd1bf3a6350f8
Will Olson <will.olson11@gmail.com>
2017-09-05 17:08:59 -0700

    feat: Add universal convert function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 // Project Assets
 import * as base from './baseConvertions';
 
-export const convertParams = (input) => (
+const fromTypes = {
+  'camel': base.fromCamel,
+  'constant': base.fromConstant,
+  'pascal': base.fromPascal,
+  'snake': data => data,
+};
+
+const toTypes = {
+  'camel': base.toCamel,
+  'constant': base.toConstant,
+  'pascal': base.toPascal,
+  'snake': data => data,
+  'trim': base.toTrim,
+};
+
+export const convert = (input, from, to) => (
+  toTypes[to](fromTypes[from](input))
+);
+
+export const convertParams = (input, from='camel', to='snake') => (
   Object.keys(input).reduce((obj, key) => {
-    obj[base.fromCamel(key)] = input[key]
+    obj[convert(key, from, to)] = input[key]
     return obj
   }, {})
 );
