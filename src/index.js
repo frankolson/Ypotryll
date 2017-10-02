743732ceecac1886df7ea4786b930520c0fc0d25
Will Olson <will.olson11@gmail.com>
2017-10-02 15:01:49 -0700

    feat: Recursively convert parameter object

--- src/index.js
+++ src/index.js
@@ -20,9 +20,19 @@ export const convert = (input, from, to) => (
   toTypes[to](fromTypes[from](input))
 );
 
-export const convertParams = (input, from='camel', to='snake') => (
-  Object.keys(input).reduce((obj, key) => {
-    obj[convert(key, from, to)] = input[key]
-    return obj
-  }, {})
-);
+export const convertParams = (input, from='camel', to='snake') => {
+  if (Array.isArray(input)) {
+    return input.map(value => convertParams(value, from, to));
+  } else {
+    return Object.keys(input).reduce((obj, key) => {
+      if (Array.isArray(input[key])) {
+        obj[convert(key, from, to)] = input[key].map(value => convertParams(value, from, to));
+      } else if (typeof input[key] == "object" && input[key] !== null) {
+        obj[convert(key, from, to)] = convertParams(input[key], from, to);
+      } else {
+        obj[convert(key, from, to)] = input[key];
+      }
+      return obj;
+    }, {})
+  }
+};
